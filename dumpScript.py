1223f76e782c80846db4d5185d80402414ed065b
Aditya Tiwari <aditya.tiwari@tact.ai>
2023-06-11 18:49:12 +0530

    added logic to publish message to kafka after tailing entries from oplog

--- dumpScript.py
+++ dumpScript.py
@@ -2,16 +2,16 @@ import pymongo
 import random
 import uuid
 
-myClient = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-myDB = myClient["dumpdb"]
-eventLogCol = myDB["event_logs"]
-objectMappingCol = myDB["object_mapping"]
+my_db = client["dumpdb"]
+event_log_col = my_db["event_logs"]
+object_mapping_col = my_db["object_mapping"]
 
-totalRowsInserted = 0
-numberOfRecords = random.randint(100, 200)
+total_rows_inserted = 0
+number_of_records = random.randint(100, 200)
 
-for i in range(0, numberOfRecords):
+for i in range(0, number_of_records):
     x = uuid.uuid1()
 
     id = "id_{}"
@@ -20,19 +20,19 @@ for i in range(0, numberOfRecords):
     meeting_id = meeting_id.format(x)
     presentation_id = "presentation_{}"
     presentation_id = presentation_id.format(x)
-    duration = random.randint(100,999)
+    duration = random.randint(100, 999)
 
-    myDict = { "id": id, "meeting_id": meeting_id, "presentation_id": presentation_id, "duration": duration }
-    eventLogCol.insert_one(myDict)
-    totalRowsInserted += 1
+    my_dict = {"id": id, "meeting_id": meeting_id, "presentation_id": presentation_id, "duration": duration}
+    event_log_col.insert_one(my_dict)
+    total_rows_inserted += 1
 
-    call2Id = "call2_{}"
-    call2Id = call2Id.format(x)
-    myDict = {"meeting_id": meeting_id, "call2_id": call2Id}
-    objectMappingCol.insert_one(myDict)
-    totalRowsInserted += 1
-    print("{} - inserted - {}".format(totalRowsInserted, meeting_id))
+    call2_id = "call2_{}"
+    call2_id = call2_id.format(x)
+    my_dict = {"meeting_id": meeting_id, "call2_id": call2_id}
+    object_mapping_col.insert_one(my_dict)
+    total_rows_inserted += 1
+    print("{} - inserted - {}".format(total_rows_inserted, meeting_id))
 
-print(myClient.list_database_names())
-print(myDB.list_collection_names())
-print("total rows inserted - {}".format(totalRowsInserted))
+print(client.list_database_names())
+print(my_db.list_collection_names())
+print("total rows inserted - {}".format(total_rows_inserted))
--- tailOplog.py
+++ tailOplog.py
@@ -1,33 +1,55 @@
-import pymongo
+import json
+import pickle
 from pymongo import MongoClient
 from pymongo.cursor import CursorType
-
-client = MongoClient('mongodb://localhost:27017')
-
-# Execute the replSetGetStatus command
-# result = client.admin.command('replSetGetStatus')
-
-# Check the number of replica sets
-# num_replica_sets = len(result['members'])
-# print("Number of replica sets:", num_replica_sets)
-
-local_db = client.local
-oplog = local_db['oplog.rs']
-cursor = oplog.find({}, cursor_type=CursorType.TAILABLE_AWAIT)
-
-while cursor.alive:
-    try:
-        for entry in cursor:
-            print(entry)
-    except StopIteration:
-        print('stop iteration occured no more data')
-        pass
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-client.close()
+from confluent_kafka.admin import AdminClient
+from confluent_kafka import Producer
+from bson import Binary
 
 
+def publish_message(bootstrap_servers, topic, message):
+    producer = Producer({"bootstrap.servers": bootstrap_servers})
+    producer.produce(topic=topic, value=message)
+    producer.flush()
 
 
+def topic_exists(bootstrap_servers, topic):
+    admin_client = AdminClient({"bootstrap.servers": bootstrap_servers})
+    topics = admin_client.list_topics().topics
+    print('topics {}'.format(topics))
+    exists = topic in topics
+    return exists
 
+
+def main():
+    client = MongoClient('mongodb://localhost:27017')
+    local_db = client.local
+    oplog = local_db['oplog.rs']
+    cursor = oplog.find({}, cursor_type=CursorType.TAILABLE_AWAIT)
+
+    bootstrap_servers = "localhost:9092"
+    topic = "mongo-oplog"
+
+    exists = topic_exists(bootstrap_servers, topic)
+    if exists:
+        print(f"The topic '{topic}' exists in Kafka.")
+    else:
+        print(f"The topic '{topic}' does not exist in Kafka.")
+        raise Exception
+
+    while cursor.alive:
+        try:
+            for entry in cursor:
+                print(f"{entry['op']}")
+                publish_message(bootstrap_servers, topic, pickle.dumps(entry))
+        except StopIteration:
+            print('Stop iteration occurred, no more data')
+            pass
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
